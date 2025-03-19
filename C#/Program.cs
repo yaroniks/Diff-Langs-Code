c83afeb5df72a4bd28834c107e66ad7561e4aa28
yarovich <anokhin247yarik0@gmail.com>
2025-03-19 17:36:43 +0300

    add C#

---
+++ C#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+
+class Program {
+    static void Main() {
+        var start = Stopwatch.StartNew();
+        string data = File.ReadAllText("messages.json");
+        using JsonDocument doc = JsonDocument.Parse(data);
+        JsonElement root = doc.RootElement;
+        JsonElement messagesArray = root.GetProperty("messages");
+        var messages = new List<JsonElement>();
+        foreach (JsonElement element in messagesArray.EnumerateArray()) {
+            messages.Add(element);
+        }
+        messages.Reverse();
+        var temp_msgs = new Dictionary<long, string>();
+        foreach (var message in messages) {
+            long id = message.GetProperty("id").GetInt64();
+            string content = message.GetProperty("content").GetString();
+            temp_msgs[id] = content;
+        }
+        var answers = new Dictionary<string, List<string>>();
+
+        foreach (var message in messages) {
+            if (message.TryGetProperty("reply_message_id", out JsonElement replyIdElement) && replyIdElement.ValueKind != JsonValueKind.Null) {
+                long replyId = replyIdElement.GetInt64();
+                if (temp_msgs.TryGetValue(replyId, out string text)) {
+                    if (!answers.ContainsKey(text))
+                        answers[text] = new List<string>();
+                    answers[text].Add(message.GetProperty("content").GetString());
+                }
+            }
+        }
+
+        string outputJson = JsonSerializer.Serialize(new { answers }, new JsonSerializerOptions { WriteIndented = false, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
+        File.WriteAllText("answers.json", outputJson);
+        
+        start.Stop();
+        Console.WriteLine(start.Elapsed.TotalSeconds);
+    }
+}
